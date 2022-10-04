23a7e538e8556ad87a710c1824b252972c33299f
RafaHdzCh <64990211+RafaHdzCh@users.noreply.github.com>
2022-10-04 14:43:20 -0500

    E03 Interface and turrets

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private int waveIndex = 0;
+    [SerializeField] TextMeshProUGUI waveCountdownText;
+
+    private void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Floor(countdown).ToString();
+    }
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
